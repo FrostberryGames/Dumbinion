83caef78feaff9463b2a0ae96385c3e4b9f86c57
Tristan St-Gelais <tristanstgelais20@gmail.com>
2026-01-01 11:24:49 -0500

    Small tweaks and changed some images

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -5,13 +5,8 @@ signal host_game(username:String)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if OS.is_debug_build():
+		$Ip.text="localhost"
 
 func get_username():
 	return $LineEdit.text
